d2e5c12cc354af7de7e66281a3d79b26128be2f7
Steven Sanchez <steven12661@gmail.com>
2021-09-24 15:53:54 -0500

    Added navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, Icon } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import logo from '../../assets/e-commerce-logo.png';
+import useStyles from './navbarStyles';
+
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title} color="inherit">
+                        <img src={logo} alt="Commmerce.js" height="25px" className={classes.image} />
+                        E-Commerce
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton  aria-label= "Show cart items" color="inherit">
+                            <Badge badgeContent={2} color="secondary">
+                            <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import Product from "./Product/Product";
 
+import Product from "./Product/Product";
+import usseStyles from "./productsStyles";
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image:'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/u7khoqev6hy2xgsllrnb/revolution-5-running-shoes-szF7CS.png' },
     {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image:'https://tiendasishop.com/media/catalog/product/m/y/mydc2e_a.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=700&width=700&canvas=700:700' }
 
 ]
 const Products = () => {
+    const classes = usseStyles();
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
         <Grid container justify="center" spacing={4}>
             {products.map((product)=> (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
